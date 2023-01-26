104fb8658e204158281bf8fd570dd31300683241
BojanIstvancic <bojan.istvancic@gmail.com>
2023-01-26 13:15:08 +0100

    add tests for popover and add popover in the code

--- src/pages/summary/test/SummaryForm.test.jsx
+++ src/pages/summary/test/SummaryForm.test.jsx
@@ -2,6 +2,28 @@ import { render, screen } from "@testing-library/react";
 import SummaryForm from "../SummaryForm";
 import userEvent from "@testing-library/user-event";
 
+/*
+//  command[ALL]ByQueryType
+
+  COMMANDS
+ - get: expect element to be in DOM
+ - query: expect element not to be in DOM
+ - find: expect element to appear async
+
+ [ALL]
+ - (exclude) - expects one match
+ - (include) - expects more than one match
+
+ QueryType
+ - Role
+ - AltText
+ - Text
+ - Form
+    - PlaceholderText
+    - LabelText
+    - DisplayValue
+*/
+
 test("initial setup", () => {
   render(<SummaryForm />);
 
@@ -35,10 +57,23 @@ test("check if button is enabled on first click and disabled on second", async (
 
 test("popover responds to hover", async () => {
   const user = userEvent.setup();
+  render(<SummaryForm />);
 
   // popover starts out hidden
+  const nullPopover = screen.queryByText(
+    /no ice cream will actually be delivered/i
+  );
+
+  expect(nullPopover).not.toBeInTheDocument();
 
   // popover appears on mouseover of checkbox label
+  const termsAndConditions = screen.getByText(/terms and conditions/i);
+  await user.hover(termsAndConditions);
+
+  const popover = screen.getByText(/no ice cream will actually be delivered/i);
+  expect(popover).toBeInTheDocument();
 
   // popover disappears when we mouse out
+  await user.unhover(termsAndConditions);
+  expect(popover).not.toBeInTheDocument();
 });
